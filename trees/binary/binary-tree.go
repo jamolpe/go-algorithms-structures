ff1890899b3cc09ff3ca34d39cf4c43b8e30afac
jamolpe <jamolpe@gmail.com>
2020-10-18 00:56:59 +0200

    feat: delete

--- trees/binary/binary-tree.go
+++ trees/binary/binary-tree.go
@@ -32,6 +32,37 @@ func (t *BinaryTree) Insert(val interface{}) {
 	}
 }
 
+func findMinFromNode(pretree **BinaryTree) interface{} {
+	tree := *pretree
+	if tree.left.data == nil {
+		value := tree.data
+		fmt.Printf("found value %v \n", value)
+		if tree.right.data != nil {
+			*pretree = tree.right
+		} else {
+			tree.data = nil
+		}
+		return value
+	} else {
+		fmt.Printf("node value: %v, leftValue: %v not found next one\n", tree.data, tree.left.data)
+		return findMinFromNode(&tree.left)
+	}
+}
+
+func (t *BinaryTree) Remove(val interface{}) {
+	if val == t.data {
+		if t.right == nil && t.left == nil {
+			t.data = nil
+		} else if t.left != nil && t.right == nil {
+			t.data = t.left.data
+		} else if t.right != nil && t.left == nil {
+			t.data = t.right.data
+		} else {
+			t.data = findMinFromNode(&t.right)
+		}
+	}
+}
+
 func (t *BinaryTree) Print(deep int, ch string) {
 	if t == nil {
 		return
@@ -42,14 +73,3 @@ func (t *BinaryTree) Print(deep int, ch string) {
 	t.left.Print(deep, "L")
 	t.right.Print(deep, "R")
 }
-
-func (t *BinaryTree) Remove(val interface{}) {
-	if t.data === val {
-		t.data = nil
-		if
-	}
-}
-
-// func (t *BinaryTree) Search(val interface{}) *BinaryTree {
-
-// }
